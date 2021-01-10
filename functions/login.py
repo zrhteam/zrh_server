85d14900e4a90f14073a8d4965adc0b5e4282b9d
cjzzy <11711402@mail.sustech.edu.cn>
2021-01-10 21:49:16 +0800

    重构了后端框架

--- functions/login.py
+++ functions/login.py
@@ -9,26 +9,100 @@ login_blueprint = Blueprint('login', __name__, url_prefix='/login')
 def get_grant():
     print("In function get_grant")
     start_t = datetime.now()
-    name = request.form.get("name")
-    password = request.form.get("pwd")
+    name = request.values.get("name")
+    password = request.values.get("pwd")
     print("Received name: " + str(name))
     print("Received password: " + str(password))
     flag = 0
     resp_data = {"code": 0,
-                 "msg": {"user_grant": "",
-                         "headquarter_tag": "",
-                         "region_tag": "",
-                         "project_tag": ""}}
+                 "data": {"user_grant": "",
+                          "headquarter_tag": "",
+                          "region_tag": "",
+                          "project_tag": "",
+                          "value": {}}}
     cache_risk_user = gl.get_value("cache_risk_user")
+    cache_prj_with_tag = gl.get_value("cache_prj_with_tag")
+    cache_check_location_map = gl.get_value("cache_check_location_map")
     for item in cache_risk_user:
         if item.name == name:
             if item.password == password:
-                resp_data["msg"]["user_grant"] = item.user_grant
-                resp_data["msg"]["headquarter_tag"] = item.headquarter_tag
-                resp_data["msg"]["region_tag"] = item.region_tag
-                resp_data["msg"]["project_tag"] = item.project_tag
+                resp_data["data"]["user_grant"] = item.user_grant
+                resp_data["data"]["headquarter_tag"] = item.headquarter_tag
+                resp_data["data"]["region_tag"] = item.region_tag
+                resp_data["data"]["project_tag"] = item.project_tag
+                value = {}
+                if item.user_grant == "超级用户":
+                    value["headquarter_tag"] = {}
+                    for ele in cache_prj_with_tag:
+                        if ele.headquarter_tag not in value["headquarter_tag"].keys():
+                            value["headquarter_tag"][ele.headquarter_tag]["region_tag"] = {}
+                        if ele.region_tag is None:
+                            if "project_tag" not in value["headquarter"][ele.headquarter_tag].keys():
+                                value["headquarter"][ele.headquarter_tag]["project_tag"] = {}
+                            if ele.project_tag is None: # 如果project都为null，那么可以判断为测试项目，跳过
+                                resp_data["code"] = -1
+                                break
+                            if ele.project_tag not in value["headquarter"][ele.headquarter_tag]["project_tag"].keys():
+                                value["headquarter"][ele.headquarter_tag]["project_tag"][ele.project_tag] = []
+                            value["headquarter"][ele.headquarter_tag]["project_tag"][ele.project_tag].append(cache_check_location_map[ele.project_name])
+                        else:
+                            if ele.region_tag not in value["headquarter_tag"][ele.headquarter_tag]["region_tag"].keys():
+                                value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"] = {}
+                            if ele.project_tag is None: # 如果project都为null，那么可以判断为测试项目，跳过
+                                resp_data["code"] = -1
+                                break
+                            if ele.project_tag not in value["headquarter"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"].keys():
+                                value["headquarter"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"][ele.project_tag] = []
+                            value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"][ele.project_tag].append(cache_check_location_map[ele.project_name])
+                elif item.user_grant == "总部":
+                    value["headquarter_tag"][item.headquarter_tag]["region"] = {}
+                    for ele in cache_prj_with_tag:
+                        if ele.headquarter_tag == item.headquarter_tag:
+                            if ele.region_tag is None:
+                                if "project_tag" not in value["headquarter"][item.headquarter_tag].keys():
+                                    value["headquarter"][item.headquarter_tag]["project_tag"] = {}
+                                if ele.project_tag is None:  # 如果project都为null，那么可以判断为测试项目，跳过
+                                    resp_data["code"] = -1
+                                    break
+                                if ele.project_tag not in value["headquarter"][item.headquarter_tag][
+                                    "project_tag"].keys():
+                                    value["headquarter"][item.headquarter_tag]["project_tag"][ele.project_tag] = []
+                                value["headquarter"][item.headquarter_tag]["project_tag"][ele.project_tag].append(cache_check_location_map[ele.project_name])
+                            else:
+                                if ele.region_tag not in value["headquarter_tag"][ele.headquarter_tag][
+                                    "region_tag"].keys():
+                                    value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag][
+                                        "project_tag"] = {}
+                                if ele.project_tag is None:  # 如果project都为null，那么可以判断为测试项目，跳过
+                                    resp_data["code"] = -1
+                                    break
+                                if ele.project_tag not in value["headquarter"][item.headquarter_tag]["region_tag"][ele.region_tag][
+                                    "project_tag"].keys():
+                                    value["headquarter"][item.headquarter_tag]["project_tag"][ele.project_tag] = []
+                                value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag][
+                                    "project_tag"][ele.project_tag].append(cache_check_location_map[ele.project_name])
+                elif item.user_grant == "区域":
+                    value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"] = {}
+                    for ele in cache_prj_with_tag:
+                        if ele.project_tag == item.project_tag:
+                            if ele.project_tag not in value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"].keys():
+                                value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][ele.project_tag] = []
+                            value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][
+                                ele.project_tag].append(cache_check_location_map[ele.project_name])
+                elif item.user_grant == "项目":
+                    value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][item.project_tag] = []
+                    for ele in cache_prj_with_tag:
+                        if ele.project_tag == item.project_tag:
+                            value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][
+                                item.project_tag].append(cache_check_location_map[ele.project_name])
+                else:
+                    resp_data["code"] = -1
                 flag = 1
             else:
                 resp_data["code"] = -1
             break
+    print("Returned data: ")
+    print(resp_data)
+    end_t = datetime.now()
+    print(print("Query total time is: " + str((end_t - start_t).seconds) + "s"))
     return jsonify(resp_data)
