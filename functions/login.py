0d0cca5cde5f15d9a91a9cf82cbbf248accc43fe
12032465 <12032465@mail.sustech.edu.cn>
2021-01-11 12:05:00 +0800

    1.11

--- functions/login.py
+++ functions/login.py
@@ -2,15 +2,15 @@ from flask import Blueprint, jsonify, request, render_template, session, json
 from datetime import datetime
 import functions.cache_data as gl
 
-login_blueprint = Blueprint('login', __name__, url_prefix='/login')
+login_blueprint = Blueprint('login', __name__, url_prefix='/api/login')
 
 
 @login_blueprint.route('/get_grant', methods=['POST', 'GET'])
 def get_grant():
     print("In function get_grant")
     start_t = datetime.now()
-    name = request.values.get("name")
-    password = request.values.get("pwd")
+    name = request.values.get("username")
+    password = request.values.get("password")
     print("Received name: " + str(name))
     print("Received password: " + str(password))
     flag = 0
@@ -23,6 +23,8 @@ def get_grant():
     cache_risk_user = gl.get_value("cache_risk_user")
     cache_prj_with_tag = gl.get_value("cache_prj_with_tag")
     cache_check_location_map = gl.get_value("cache_check_location_map")
+    print(cache_check_location_map)
+    print(len(cache_check_location_map))
     for item in cache_risk_user:
         if item.name == name:
             if item.password == password:
@@ -34,73 +36,90 @@ def get_grant():
                 if item.user_grant == "超级用户":
                     value["headquarter_tag"] = {}
                     for ele in cache_prj_with_tag:
+                        if ele.headquarter_tag is None:
+                            continue
                         if ele.headquarter_tag not in value["headquarter_tag"].keys():
-                            value["headquarter_tag"][ele.headquarter_tag]["region_tag"] = {}
+                            value["headquarter_tag"][ele.headquarter_tag] = {"region_tag": {}}
                         if ele.region_tag is None:
-                            if "project_tag" not in value["headquarter"][ele.headquarter_tag].keys():
-                                value["headquarter"][ele.headquarter_tag]["project_tag"] = {}
+                            if "project_tag" not in value["headquarter_tag"][ele.headquarter_tag].keys():
+                                value["headquarter_tag"][ele.headquarter_tag]["project_tag"] = {}
                             if ele.project_tag is None: # 如果project都为null，那么可以判断为测试项目，跳过
                                 resp_data["code"] = -1
                                 break
-                            if ele.project_tag not in value["headquarter"][ele.headquarter_tag]["project_tag"].keys():
-                                value["headquarter"][ele.headquarter_tag]["project_tag"][ele.project_tag] = []
-                            value["headquarter"][ele.headquarter_tag]["project_tag"][ele.project_tag].append(cache_check_location_map[ele.project_name])
+                            if ele.project_tag not in value["headquarter_tag"][ele.headquarter_tag]["project_tag"].keys():
+                                value["headquarter_tag"][ele.headquarter_tag]["project_tag"][ele.project_tag] = []
+                            print(ele.project_code)
+                            if ele.project_code in cache_check_location_map.keys():
+                                value["headquarter_tag"][ele.headquarter_tag]["project_tag"][ele.project_tag].append({ele.project_code: cache_check_location_map[ele.project_code]})
                         else:
                             if ele.region_tag not in value["headquarter_tag"][ele.headquarter_tag]["region_tag"].keys():
-                                value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"] = {}
+                                value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag] = {"project_tag": {}}
                             if ele.project_tag is None: # 如果project都为null，那么可以判断为测试项目，跳过
                                 resp_data["code"] = -1
                                 break
-                            if ele.project_tag not in value["headquarter"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"].keys():
-                                value["headquarter"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"][ele.project_tag] = []
-                            value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"][ele.project_tag].append(cache_check_location_map[ele.project_name])
+                            if ele.project_tag not in value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"].keys():
+                                value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"][ele.project_tag] = []
+                            print(ele.project_code)
+                            if ele.project_code in cache_check_location_map.keys():
+                                value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"][ele.project_tag].append({ele.project_code: cache_check_location_map[ele.project_code]})
                 elif item.user_grant == "总部":
-                    value["headquarter_tag"][item.headquarter_tag]["region"] = {}
+                    value["headquarter_tag"] = {item.headquarter_tag: {"region_tag": {}}}
                     for ele in cache_prj_with_tag:
                         if ele.headquarter_tag == item.headquarter_tag:
                             if ele.region_tag is None:
-                                if "project_tag" not in value["headquarter"][item.headquarter_tag].keys():
-                                    value["headquarter"][item.headquarter_tag]["project_tag"] = {}
+                                if "project_tag" not in value["headquarter_tag"][item.headquarter_tag].keys():
+                                    value["headquarter_tag"][item.headquarter_tag]["project_tag"] = {}
                                 if ele.project_tag is None:  # 如果project都为null，那么可以判断为测试项目，跳过
                                     resp_data["code"] = -1
                                     break
-                                if ele.project_tag not in value["headquarter"][item.headquarter_tag][
+                                if ele.project_tag not in value["headquarter_tag"][item.headquarter_tag][
                                     "project_tag"].keys():
-                                    value["headquarter"][item.headquarter_tag]["project_tag"][ele.project_tag] = []
-                                value["headquarter"][item.headquarter_tag]["project_tag"][ele.project_tag].append(cache_check_location_map[ele.project_name])
+                                    value["headquarter_tag"][item.headquarter_tag]["project_tag"][ele.project_tag] = []
+                                print(ele.project_code)
+                                if ele.project_code in cache_check_location_map.keys():
+                                    value["headquarter_tag"][item.headquarter_tag]["project_tag"][ele.project_tag].append({ele.project_code: cache_check_location_map[ele.project_code]})
                             else:
                                 if ele.region_tag not in value["headquarter_tag"][ele.headquarter_tag][
                                     "region_tag"].keys():
-                                    value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag][
-                                        "project_tag"] = {}
+                                    value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag] = {"project_tag": {}}
                                 if ele.project_tag is None:  # 如果project都为null，那么可以判断为测试项目，跳过
-                                    resp_data["code"] = -1
-                                    break
-                                if ele.project_tag not in value["headquarter"][item.headquarter_tag]["region_tag"][ele.region_tag][
+                                    continue
+                                if ele.project_tag not in value["headquarter_tag"][item.headquarter_tag]["region_tag"][ele.region_tag][
                                     "project_tag"].keys():
-                                    value["headquarter"][item.headquarter_tag]["project_tag"][ele.project_tag] = []
-                                value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag][
-                                    "project_tag"][ele.project_tag].append(cache_check_location_map[ele.project_name])
+                                    value["headquarter_tag"][item.headquarter_tag]["region_tag"][ele.region_tag]["project_tag"][ele.project_tag] = []
+                                print(ele.project_code)
+                                if ele.project_code in cache_check_location_map.keys():
+                                    value["headquarter_tag"][ele.headquarter_tag]["region_tag"][ele.region_tag][
+                                    "project_tag"][ele.project_tag].append({ele.project_code: cache_check_location_map[ele.project_code]})
                 elif item.user_grant == "区域":
-                    value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"] = {}
+                    value["headquarter_tag"] = {item.headquarter_tag: {"region": {item.region_tag: {"project_tag": {}}}}}
                     for ele in cache_prj_with_tag:
-                        if ele.project_tag == item.project_tag:
+                        if ele.region_tag == item.region_tag:
+                            print(ele.project_tag)
                             if ele.project_tag not in value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"].keys():
                                 value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][ele.project_tag] = []
-                            value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][
-                                ele.project_tag].append(cache_check_location_map[ele.project_name])
+                            print(ele.project_code)
+                            if ele.project_code in cache_check_location_map.keys():
+                                value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][
+                                ele.project_tag].append({ele.project_code: cache_check_location_map[ele.project_code]})
                 elif item.user_grant == "项目":
-                    value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][item.project_tag] = []
+                    value["headquarter_tag"] = {item.headquarter_tag: {"region": {item.region_tag: {"project_tag": {item.project_tag: []}}}}}
                     for ele in cache_prj_with_tag:
                         if ele.project_tag == item.project_tag:
-                            value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][
-                                item.project_tag].append(cache_check_location_map[ele.project_name])
+                            print(ele.project_code)
+                            if ele.project_code in cache_check_location_map.keys():
+                                value["headquarter_tag"][item.headquarter_tag]["region"][item.region_tag]["project_tag"][
+                                item.project_tag].append({ele.project_code: cache_check_location_map[ele.project_code]})
                 else:
                     resp_data["code"] = -1
+                resp_data["data"]["value"] = value
                 flag = 1
+
             else:
                 resp_data["code"] = -1
             break
+    if resp_data["data"]["user_grant"] == "":
+        resp_data["code"] = -1
     print("Returned data: ")
     print(resp_data)
     end_t = datetime.now()
--- run.py
+++ run.py
@@ -4,10 +4,11 @@ from models.new_cascade_risk_prj_danger_record import NewCascadeRiskPrjDangerRec
 from models.risk_user import RiskUser
 from models.project_with_tag import PrjWithTag
 import functions.cache_data as gl
+from flask_cors import CORS
 
 app = app_create('testing')
-
-
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, supports_credentials=True)
 @app.before_first_request
 def cache_tables():
     print("In func cache_tables")
@@ -29,21 +30,20 @@ def cache_tables():
     print(cache_prj_with_tag[0])
 
     # 缓存表 new_cascade_risk_prj_danger_record
-    # cache_cascade_record = NewCascadeRiskPrjDangerRecord.query.all()
-    # end_t3 = datetime.now()
-    # gl.set_value("cache_cascade_record", cache_cascade_record)
-    # print("Time to query table 3 is " + str((end_t3 - end_t2).seconds) + "s")
-    # print(cache_cascade_record[0])
-    #
-    # cache_check_location_map = {}
-    # for item in cache_cascade_record:
-    #     if item.project_name not in cache_check_location_map.keys():
-    #         cache_check_location_map[item.project_name] = {"lat": item.lat, "lng": item.lng}
-    # gl.set_value("cache_check_location_map", cache_check_location_map)
+    cache_cascade_record = NewCascadeRiskPrjDangerRecord.query.all()
+    end_t3 = datetime.now()
+    gl.set_value("cache_cascade_record", cache_cascade_record)
+    print("Time to query table 3 is " + str((end_t3 - end_t2).seconds) + "s")
+
+    cache_check_location_map = {}
+    for item in cache_cascade_record:
+        if item.project_code not in cache_check_location_map.keys():
+            cache_check_location_map[item.project_code] = {"lat": item.lat, "lng": item.lng}
+    gl.set_value("cache_check_location_map", cache_check_location_map)
 
     end_t = datetime.now()
     print("Time to query all is " + str((end_t - start_t).seconds) + "s")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)
